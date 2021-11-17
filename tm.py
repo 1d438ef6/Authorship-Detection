412981f144d4ffe788514f9816f0feb2dd01f26e
lwutke <lwutke@hs-mittweida.de>
2021-11-17 12:43:07 +0100

    alles mögliche

--- tm.py
+++ tm.py
@@ -256,6 +256,10 @@ class tm:
             return -1
         h = tm.get_symbol_frequency(text = text, symbols = symbol)[0]
         return h/tm.get_number_of_sentences(text)
+    def get_sentence_complexity2(text = None, symbol = ",", replace = False):            #gibt die Satzkomplexität zurück
+        if text == None:
+            return -1
+        return tm.get_sentence_complexity(text=text,symbol=symbol,replace=replace) * (sum(tm.get_symbol_frequency(text=text))/tm.get_number_of_sentences(text=text,replace=replace))
     def get_number_of_filler_words(text = None, filler_words = ["von","der","die","das","aber"]):     #gibt die Anzahl der gegebenen Füllwörter zurück 
         if text == None or len(filler_words)<1:
             return -1
